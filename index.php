1d712f2a690c799ba4dfe06f994801bab7919479
Leysan@LAPTOP-25B9S5T6 <leisan.nasretdinova@gmail.com>
2025-06-09 15:40:22 +0200

    dtyje67jtf

---
+++ index.php
@@ -0,0 +1,83 @@
+//<?php
+
+
+//$stringa = 'Ciao'; //un solo uguale, assegnazione
+
+//const INTERO = 78; // senza dollaro,costanti IN MAIUSCOLO
+
+
+//$testoNuovoDaScrivere = 'ciao'; //camelcase (usare per variabili)
+//$testo_nuovo_da_compilare ='ciao'; //snake case (usare per nomi dei file)
+
+
+//come non scrivere variabili
+//$2ciao = 'lorenzo';
+//$_ciao = 'lorenzo';
+//$ci.ao = 'lorenzo';
+//$ ciaociao = 'lorenzo';
+
+//echo $stringa //stampa
+
+//1) Predefiniti, scalari o primitivi
+//2) composti o user defined
+
+//$stringa = 'ciao'; //stringa
+//$intero = 67890; //intero
+//$float = 56.90; // float (punti, non virgola)
+//$booleano = true; //vero o falso
+//$null = null;
+
+
+//$parte1 ='oggi fa caldo' . "\n"; // come andare a capo
+//$parte2 ='speriamo che domani sia piu fresco';
+
+//echo $parte1 . $parte2;
+
+//dati complessi o user defined
+//$scatola = []; //Definizione di array vuoto
+//$scatola1 = ['ciao', 'sono', 'io'];
+//$scatola2 = [8787, 92827];
+
+//$scatola3 = ['sciojdis','ssx', 44, 34.09];
+
+//array chiave valore o associativo
+//$person1 = [
+//'name'=> 'Lorenzo',
+//'surname' => 'Di Raco'
+//];
+
+//echo $person1['surname'];
+
+//mixed
+//$mixed = [
+//'name' => 'Lorenzo',
+//89 =>'Gama',
+//34
+
+//];
+//echo $mixed;
+
+
+
+// echo stampa solo numeri primitivi
+
+//print r stampa tutto ma per debug
+
+// tipi di operatori
+//1) matematico
+//2) confronto
+//3) logico
+
+//1)echo $x + $y;
+//echo $x - $y;
+//echo $x * $y;
+//echo $x / $y;
+//echo $x % $y;(modulo resto);
+
+//echo $a == $b;//confronta il valore
+//echo $a === $b;//confronta il valore e il tipo
+
+<?php
+  $name = "Leysan";
+  echo "Hello, $name!";
+?>
