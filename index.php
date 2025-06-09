a257a728226e05ed82ec5ebb0d9aa5ae0ed3f402
Leysan@LAPTOP-25B9S5T6 <leisan.nasretdinova@gmail.com>
2025-06-09 17:11:57 +0200

    fsegggaew

--- index.php
+++ index.php
@@ -89,3 +89,70 @@ $person1 = [
 
 echo $person1['surname'];
 
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = 'hai';
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+echo $text1, $text2, $text3, $text4, $text5, $text6, $text7, $text8;
+
+<?php
+// Creare una variabile di tipo Stringa chiamata $results che stampi sul terminale il testo (rispettando spazi e punteggiature) di questi due array.
+
+// Ricordando l’accesso agli array con indici e chiavi visti a lezione, ricostruire questa frase:
+
+// **Nel mezzo del cammin di nostra vita mi ritrovai per una selva oscura, ché la diritta via era smarrita.**
+
+// ATTENZIONE: Nell’array potreste trovare degli intrusi o non trovare alcune preposizioni. Approcciate il problema nel modo che ritenute più opportuno: nuove variabili, concatenamenti, apice singolo, doppio apice ecc..
+
+
+$words1 = [
+    'una',
+    67,
+    'vita',
+    'colle',
+    'mi',
+    'rosso',
+    [
+        'oscura',
+        'era',
+        89,
+        [
+            'mezzo',
+            [
+                'cammin',
+                'Nel',
+                [
+                    'selva',
+                    'la',
+                    [
+                        'via',
+                        'una',
+                        true,
+                    ]
+                ],
+            ]
+        ],
+        'ritrovai',
+        'per'
+    ],
+    'diritta'
+];
+
+$words2 = [
+    'elemento1' => 25.89,
+    'elemento2' => 'nostra',
+    'elemento3' => [
+        'Virgilio',
+        'smarrita',
+        'ché'
+    ]
+];
+
+var_dump ($words2['elemento3'][1]);
+$results =  ""; //<--- Tutto in questa variabile
+echo $result;
--- kjnkhvgjh.php
+++ kjnkhvgjh.php
@@ -11,3 +11,4 @@ $text7 = 'bevuto';
 $text8 = "tutto";
 
 echo $text1, $text2, $text3, $text4, $text5, $text6, $text7, $text8;
+?>
